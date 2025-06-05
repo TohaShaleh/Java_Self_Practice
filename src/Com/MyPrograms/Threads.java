274be7d504d20783e73cd25e87bc0fd959f43345
Abu Shaleh <abushalehtoha@gmail.com>
2025-06-05 10:54:25 +0600

    Threads Basic concepts (Launch,handle etc)

---
+++ src/Com/MyPrograms/Threads.java
@@ -0,0 +1,51 @@
+package Com.MyPrograms;
+
+class test extends Thread
+{
+
+    int num;
+    public test(int num)
+    {
+        this.num=num;
+    }
+    @Override
+    public void run()
+    {
+        for(int i=0;i<8;i++)
+        {
+            System.out.println("Hello World  "+"This is from thread : "+num);
+
+            try{
+                Thread.sleep(700);
+            }
+            catch (InterruptedException e)
+            {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+}
+
+
+public class Threads{
+    public static void main(String[] args) {
+
+        test t1=new test(1);
+        test t2=new test(2);
+
+        t1.start();
+        t2.start();
+
+        try{
+            t1.join();
+            t2.join();
+        }
+        catch (InterruptedException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
